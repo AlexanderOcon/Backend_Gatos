68daf6cdb4dee318eabd95af4d65e98d1a858601
AlexanderOcon <brandon.hermida.ocon@gmail.com>
2026-08-17 11:49:36 -0600

    Cambios Backend

--- routes/gato.routes.js
+++ routes/gato.routes.js
@@ -1,8 +1,18 @@
 import express from "express";
+import multer from "multer";
 import { registrarGato } from "../controllers/gato.controller.js";
 
 const router = express.Router();
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+});
 
-router.post("/gato", registrarGato);
+router.post("/gato",
+  upload.single("imagen"),
+  registrarGato
+);
 
 export default router;
